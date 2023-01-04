f2e156888fecfada279386d4b2819099d1a2564c
Jprimero15 <jprimero155@gmail.com>
2023-01-04 04:00:00 +0000

    hwui: Silence OpenGLRenderer logspam
    this is logging every 1 to 3 seconds:
    * 01-02 11:28:23.295 E/OpenGLRenderer(9345): Unable to match the desired swap behavior.
    
    Change-Id: Id0e93f68bc8f2d0e2f5695480116a74e135846e0
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>
    (cherry picked from commit c9e6bc4eaa2a714ed04fb0484c14d9180ad60063)
    Signed-off-by: OliverSyx <173170056+OliverSyx@users.noreply.github.com>

--- libs/hwui/pipeline/skia/SkiaOpenGLPipeline.cpp
+++ libs/hwui/pipeline/skia/SkiaOpenGLPipeline.cpp
@@ -224,12 +224,12 @@ bool SkiaOpenGLPipeline::setSurface(ANativeWindow* surface, SwapBehavior swapBeh
     }
 
     if (mEglSurface != EGL_NO_SURFACE) {
-        const bool preserveBuffer = (swapBehavior != SwapBehavior::kSwap_discardBuffer);
-        const bool isPreserved = mEglManager.setPreserveBuffer(mEglSurface, preserveBuffer);
+        //const bool preserveBuffer = (swapBehavior != SwapBehavior::kSwap_discardBuffer);
+        //const bool isPreserved = mEglManager.setPreserveBuffer(mEglSurface, preserveBuffer);
         
-        if (preserveBuffer != isPreserved) {
+        //if (preserveBuffer != isPreserved) {
             //ALOGE("Unable to match the desired swap behavior.");
-        }
+        //}
         
         return true;
     }
