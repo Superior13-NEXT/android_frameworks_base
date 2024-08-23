bed579115c6da410e7ebb8c8cb773c75fff016a7
Jyotiraditya Panda <jyotiraditya@aospa.co>
2024-08-24 01:04:33 +0530

    Initial implementation of KeyboxImitationHooks
    Co-authored-by: chiteroman <querveros999@gmail.com>
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Change-Id: Id0e093cbbd095792c8925cdb48ae631bf86118ad
    Signed-off-by: Ritesh <SahanyRitesh@proton.me>
    
    base: Add the ability to generate key pairs for keybox spoofing
    
    The code is used from :-
    https://github.com/5ec1cff/TrickyStore
    
    Needs :-
    https://github.com/HELLBOY017/external_bouncycastle/commit/f70ea016239bccf2d3f4b60b79a7165ef3d047be
    
    This allows us to enjoy spoofing keyboxes the right way even with broken TEE.
    
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
    Signed-off-by: Ritesh <SahanyRitesh@proton.me>
    
    base: Use keybox spoofing only when attest key descriptor is null
    
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
    Signed-off-by: Ritesh <SahanyRitesh@proton.me>
    
    base: Allow user to provide keybox data [1/2]
    
    Handle PKCS#1 RSA keys for keybox spoofing as well
    
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
    Signed-off-by: Ritesh <SahanyRitesh@proton.me>
    
    KeyProviderManager: Accept N >= 1 certs per key [1/2]
    
    it's not always 3...
    
    base: backport keybox for android 13 compatability.
    
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>
    Signed-off-by: OliverSyx <173170056+OliverSyx@users.noreply.github.com>

--- keystore/java/android/security/KeyStore2.java
+++ keystore/java/android/security/KeyStore2.java
@@ -32,6 +32,8 @@ import android.system.keystore2.KeyEntryResponse;
 import android.system.keystore2.ResponseCode;
 import android.util.Log;
 
+import com.android.internal.util.superior.KeyboxImitationHooks;
+
 import java.util.Calendar;
 
 /**
@@ -259,6 +261,11 @@ public class KeyStore2 {
     public KeyEntryResponse getKeyEntry(@NonNull KeyDescriptor descriptor)
             throws KeyStoreException {
         StrictMode.noteDiskRead();
+
+        KeyEntryResponse response = KeyboxImitationHooks.onGetKeyEntry(descriptor);
+        if (response != null) {
+            return response;
+        }
 
         return handleRemoteExceptionWithRetry((service) -> service.getKeyEntry(descriptor));
     }
